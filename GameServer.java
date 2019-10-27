237b530504eb9ff999980a889bfe49a2553cab6c
Mridul Gain <mridulgain@gmail.com>
2019-10-27 12:31:41 +0530

    improvement : wrapper added MyStreamSocket

--- GameServer.java
+++ GameServer.java
@@ -14,26 +14,15 @@ public class GameServer{
 			ServerSocket connectionSocket = new ServerSocket(portNo);
 			System.out.println("Waiting for connection...");
 			//adding player 1
-			Socket dataSocket1 = connectionSocket.accept();
-			inStream1 = dataSocket1.getInputStream();
-			socketInput1 = new BufferedReader(new InputStreamReader(inStream1));
-			String player1 = socketInput1.readLine();
-			outStream1 = dataSocket1.getOutputStream();
-			socketOutput1 = new PrintWriter(new OutputStreamWriter(outStream1));
-			socketOutput1.println("1");//Acknowledging player 1
-			socketOutput1.flush();
+			MyStreamSocket dataSocket1 = new MyStreamSocket(connectionSocket.accept());
+			String player1 = dataSocket1.receiveMessage();
+			dataSocket1.sendMessage("1");//Acknowledging player 1
 			System.out.println("Player 1 " + player1 + " has joined....Waiting for player 2");
 			//adding player 2
-			Socket dataSocket2 = connectionSocket.accept();
-			inStream2 = dataSocket2.getInputStream();
-			socketInput2 = new BufferedReader(new InputStreamReader(inStream2));
-			String player2 = socketInput2.readLine();
-			outStream2 = dataSocket2.getOutputStream();
-			socketOutput2 = new PrintWriter(new OutputStreamWriter(outStream2));
-			socketOutput2.println(player1);//Acknowledging player 2
-			socketOutput2.flush();
-			socketOutput1.println(player2);
-			socketOutput1.flush();
+			MyStreamSocket dataSocket2 = new MyStreamSocket(connectionSocket.accept());
+			String player2 = dataSocket2.receiveMessage();
+			dataSocket2.sendMessage(player1);//Acknowledging player 2
+			dataSocket1.sendMessage(player2);
 			System.out.println("Player 2 " + player2 +" has joined....Starting the game");
 			//toss
 			Random rand = new Random();
@@ -41,12 +30,10 @@ public class GameServer{
 			char rand_ch = (char)(rand.nextInt(26) + 97);
 			String msg = rand_id + "," + rand_ch;
 			//System.out.println(msg);
-			socketOutput1.println(msg);
-			socketOutput1.flush();
-			socketOutput2.println(msg);
-			socketOutput2.flush();
-			while(1){
-				
+			dataSocket1.sendMessage(msg);
+			dataSocket2.sendMessage(msg);
+			while(true){
+				break;
 			}
 			dataSocket1.close();
 			dataSocket2.close();
@@ -58,3 +45,7 @@ public class GameServer{
 
 	}
 }
+/* To make our Game server concurrent
+class GameServerThread implements Runnable{
+
+}*/
